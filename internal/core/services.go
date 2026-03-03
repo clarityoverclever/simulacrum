c6fed071ce9e727c1638940eb1d9cef7e1b6fc1a
Keith Marshall <clarityoverclever@protonmail.com>
2026-03-02 17:17:49 -0800

    feat (core): defined service data plane

---
+++ internal/core/services.go
@@ -0,0 +1,51 @@
+// Copyright 2026 Keith Marshall
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package core
+
+import "net"
+
+type Status string
+
+const (
+	StatusRunning Status = "running"
+	StatusStopped Status = "stopped"
+	StatusError   Status = "error"
+)
+
+type ControlActon string
+
+const (
+	ActionStart   ControlActon = "start"
+	ActionStop    ControlActon = "stop"
+	ActionRestart ControlActon = "restart"
+	ActionStatus  ControlActon = "status"
+	ActionUpdate  ControlActon = "update"
+)
+
+type ControlMessage struct {
+	Action  ControlActon      `json:"action"`
+	Service string            `json:"service"`
+	Params  map[string]string `json:"params,omitempty"`
+}
+
+type ControlResponse struct {
+	Status  Status `json:"status"`
+	Message string `json:"message"`
+}
+
+type Service interface {
+	Name() string
+	Run(listener net.Listener) error
+}
